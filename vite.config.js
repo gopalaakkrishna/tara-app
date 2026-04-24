c768be9e6874d562a7a100db417c8fae9777a6de
gopalaakkrishna <gopalaakkrishna@gmail.com>
2026-04-24 13:33:07 -0400

    Simplify Vite configuration by removing Babel plugins

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [['react-refresh/babel', { skipEnvCheck: true }]],
-    }
-  })],
+  plugins: [react()],
 })
